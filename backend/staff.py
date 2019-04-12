60e4c61f610eef50d20440c2ba7f78133d2481f3
Zixiong Liu <liuzixiong@gmail.com>
2019-04-12 19:11:12 -0400

    .

--- backend/staff.py
+++ backend/staff.py
@@ -68,7 +68,7 @@ def get_orders(offset, limit):
                 "customerorders1.oid, customerorders1.date, customerorders1.status" \
                 " from (select * from customerorders left outer join staff on customerorders.eid = staff.eid) " \
                 "as customerorders1 join customers on customerorders1.cid = customers.cid " \
-                "ORDER BY customerorders1.date DESC LIMIT {} OFFSET {}".format(int(limit), int(offset)))
+                "ORDER BY customerorders1.oid DESC LIMIT {} OFFSET {}".format(int(limit), int(offset)))
         
         ret = []
         for row in result:
--- frontend/src/AddSupplier.vue
+++ frontend/src/AddSupplier.vue
@@ -1,27 +1,21 @@
 <template>
 
     <div>
-        <form class="review-form" @submit.prevent="send_form(name, address, phone_number)">
-          <p>
-            <label for="name">Name:</label>
-            <input id="name" v-model="name" placeholder="name">
-          </p>
-          
-          <p>
-            <label for="address">Address:</label>
-            <input id="address" v-model="address" placeholder="address">
-          </p>
-          
-          <p>
-            <label for="phone_number">Phone Number:</label>
-            <input id="phone_number" v-model="phone_number" placeholder="phone_number">
-          </p>
-              
-          <p>
-            <input type="submit" value="Submit">  
-          </p>    
-        
-        </form>
+        <b-card class="mt-3" header="Add supplier">
+            <b-form  class="review-form" @submit.prevent="send_form(name, address, phone_number)">
+                <b-form-group label="Supplier Name">
+                  <b-form-input id="name" v-model="name" />
+                </b-form-group>
+                <b-form-group label="Address">
+                  <b-form-input id="address" v-model="address" />
+                </b-form-group>
+                <b-form-group label="Phone Number">
+                  <b-form-input id="phone_number" v-model="phone_number" />
+                </b-form-group>
+                <b-button type="submit" variant="primary">submit</b-button>
+            
+            </b-form >
+        </b-card>
     </div>
 
 </template>
